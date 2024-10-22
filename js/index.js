aa87ff8990dc1d2ce4e3948c5c867c499f3bea86
VictoriaAguero <victoriaaguerocm@gmail.com>
2024-10-21 21:36:49 -0300

    Menú de navegación y footer, estructura general de diseño, armado de página de Nicki y Trueno

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//MENU RESPONSIVE
+const menucelu = document.querySelector("#menucelu");
+const menucompu = document.querySelector("#menucompu");
+
+   iconomenu.addEventListener("click", () => {
+       //Creamos la funcion para ocultar o mostrar segun como esta 
+       if (menucelu.style.display === "block") {
+           menucompu.style.display = "none"; // Oculta el menú compu
+       } else {
+           menucompu.style.display = "block"; // Muestra el menú compu
+       }
+   });
