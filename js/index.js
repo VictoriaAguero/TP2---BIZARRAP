c3ca04e15b732d9907988f82f6d8adda3e20866e
VictoriaAguero <victoriaaguerocm@gmail.com>
2024-10-28 20:13:58 -0300

    funcionalidad del boton de las cards de la página creadora lista, cree un nuevo archivo js

--- js/index.js
+++ js/index.js
@@ -62,28 +62,4 @@ const menucompu = document.querySelector("#menucompu");
 
    
 
-/*Pagina creadora interaccion */
 
-// Selecciono los botones
-const button1 = document.querySelector('#botoncards1');
-const button2 = document.querySelector('#botoncards2');
-const button3 = document.querySelector('#botoncards3');
-
-// Selecciono los textos
-const cardText1 = document.querySelector('#cardText1'); 
-const cardText2 = document.querySelector('#cardText2'); 
-const cardText3 = document.querySelector('#cardText3');
-
-// Se muestra o no según si ya se tocó el botón o no 
-function interaccioncardsboton(cardText) {
-    if (cardText.style.display === 'none' || cardText.style.display === '') { 
-        cardText.style.display = 'block';
-    } else {
-        cardText.style.display = 'none';
-    }
-}
-
-// Asignar eventos a cada botón
-button1.addEventListener('click', () => interaccioncardsboton(cardText1));
-button2.addEventListener('click', () => interaccioncardsboton(cardText2));
-button3.addEventListener('click', () => interaccioncardsboton(cardText3));
